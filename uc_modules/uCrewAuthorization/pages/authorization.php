cb8fb7c7accd326d55047970c038314c744073f8
stellar-creator <stelofme@gmail.com>
2022-02-02 20:47:55 +0500

    Added libraries from distributors

--- uc_modules/uCrewAuthorization/pages/authorization.php
+++ uc_modules/uCrewAuthorization/pages/authorization.php
@@ -1,3 +1,16 @@
-<?php
-	echo "123123123123123123123123123123123123";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="uc_resources/distribution/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<title></title>
+</head>
+<body>
+	Authorization form
+	<script src="uc_resources/distribution/jquery/jquery.min.js"></script>
+	<script src="uc_resources/distribution/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="uc_resources/distribution/popper/popper.js"></script>
+    <script src="uc_resources/distribution/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
